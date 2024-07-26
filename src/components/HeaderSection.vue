ecfac8b70a5c2ccbf9d544e4b95b3f3b247ff3cd
Michał Bojdo <87088719+michalbojdo@users.noreply.github.com>
2024-07-26 21:40:24 +0200

    Changed header.

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -2,28 +2,44 @@
 
 <template>
   <header>
-    <h3>
-      Produkty stolarskie
-      <h5>Artur Jurek</h5>
-    </h3>
-    
+    <q-parallax
+      class="col"
+      src="https://d2yvmenv39glx3.cloudfront.net/images/f-105323-co-robi-stolarz-wszystko-o-uslugach-stolarskich.jpg"
+      width="100%" 
+      height="200"
+    >
+      <h1 class="text-h3">
+        Produkty stolarskie
+        <h2 class="text-h4">Artur Jurek</h2>
+      </h1>
+      
+    </q-parallax>
+    <div class="q-pa-sm links" reveal elevated>
+      <h3>
+        <router-link class="text-white" to="/oferty">
+          Pełna oferta <q-icon name="arrow_right" />
+        </router-link>
+      </h3>
+      
+    </div>
   </header>
 </template>
 
 <style>
 header {
-  width: 100%;
+  width: 100%; 
   background-color: var(--secondary);
-  color: var(--white); 
-padding: 30px 10px 50px;
-
 
   * {
     font-family: "Bebas Neue";
+    color: var(--white);
   }
 }
 
-h5, h3 {
+h1,
+h2,
+h5,
+h3 {
   margin: 0;
   padding: 0;
 }
